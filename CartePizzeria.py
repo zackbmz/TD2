0a87f39a2b14332e0a8b4bec03f32a80600e6e01
zbmz9106 <zackboum.zb@gmail.com>
2023-03-06 14:28:16 +0100

    first commit

---
+++ CartePizzeria.py
@@ -0,0 +1,19 @@
+class CartePizzeria:
+    def __init__(self):
+        self.pizzas = []
+
+    def is_empty(self):
+        return len(self.pizzas) == 0
+
+    def nb_pizzas(self):
+        return len(self.pizzas)
+
+    def add_pizza(self, pizza):
+        self.pizzas.append(pizza)
+
+    def remove_pizza(self, name):
+        for i, pizza in enumerate(self.pizzas):
+            if pizza.name == name:
+                del self.pizzas[i]
+                return
+            else CartePizzeriaException(f"La pizza '{name}' n'existe pas dans la carte.")
